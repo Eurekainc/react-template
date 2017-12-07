b68c8f406ad8b45434081c141a324e2f8049d857
guzman.iglesias@moove-it.com <guzman.iglesias@moove-it.com>
2017-12-07 11:29:17 -0300

    Add basic postcss plugins to config

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,12 @@
-/* eslint-disable import/no-extraneous-dependencies */
-const postcssImport = require('postcss-import');
-const autoprefixer = require('autoprefixer');
-
-module.exports = {
-  plugins: [postcssImport, autoprefixer],
-};
-/* eslint-enable import/no-extraneous-dependencies */
+/module.exports = {
+  parser: 'postcss-scss',
+  sourceMap: 'inline',
+  plugins: {
+    'postcss-import': {},
+    'postcss-url': {},
+    'precss': {},
+    'postcss-cssnext': {
+      browsers: ['last 2 versions', '> 5%'],
+    },
+  }
+}
